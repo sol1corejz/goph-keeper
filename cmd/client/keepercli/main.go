89004ada53d71c6d0d2813af05e29bef854bc54e
ilya <sol1kek@estelink.ru>
2025-02-09 15:28:25 +0300

    версия собрки клиента

--- cmd/client/keepercli/main.go
+++ cmd/client/keepercli/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"github.com/sol1corejz/goph-keeper/cmd/client/keepercli/cmd"
 )
 
+var (
+	buildVersion = "N/A"
+	buildDate    = "N/A"
+)
+
 func main() {
+	// Вывод информации о версии сборки
+	fmt.Printf("Build version: %s\n", buildVersion)
+	fmt.Printf("Build date: %s\n", buildDate)
 	cmd.Execute()
 }
